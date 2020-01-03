ae30576f70a69942146777d21211f29c2878c617
kenabrus <kenabrus@gmail.com>
2020-01-03 21:19:53 +0100

    stworzono klase Pojazd

---
+++ DomainModels/Pojazd.cs
@@ -0,0 +1,10 @@
+namespace TrainChecklist.DomainModels
+{
+    public class Pojazd
+    {
+        public int Id {get; set;}
+        public int NumerPojazdu {get; set;}
+        public string TypPojazdu {get; set;}
+        public int IloscWagonow {get; set;}
+    }
+}
