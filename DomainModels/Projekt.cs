0027282657af6af7e62bad85de139ead45412da7
Marek <marekwas86@gmail.com>
2020-01-03 21:28:57 +0100

    utworzenie klasy Projekt

---
+++ DomainModels/Projekt.cs
@@ -0,0 +1,10 @@
+namespace TrainChecklist.DomainModels
+{
+    public class Projekt
+    {
+        public int Id {get;set;}
+        public int NazwaProjektu {get; set;}
+        public string NazwaKlienta {get; set}
+      //  public Icollection 
+    }
+}
