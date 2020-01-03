0e58d3f3fb4e74600a8e21d9206f955403267733
kenabrus <kenabrus@gmail.com>
2020-01-03 21:34:56 +0100

    dodano cos tam

--- DomainModels/Pojazd.cs
+++ DomainModels/Pojazd.cs
@@ -3,8 +3,15 @@ namespace TrainChecklist.DomainModels
     public class Pojazd
     {
         public int Id {get; set;}
-        public int NumerPojazdu {get; set;}
+        public int NumerPojazdu {get; private set;}
         public string TypPojazdu {get; set;}
         public int IloscWagonow {get; set;}
+
+        public Pojazd(){}
+
+        public void SetNumerPojazdu(int nazwa)
+        {
+            NumerPojazdu = nazwa;
+        }
     }
 }
